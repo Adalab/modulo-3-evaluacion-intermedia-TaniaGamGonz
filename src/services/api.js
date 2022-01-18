e997d2a2ff05b8175e3e85a1d79a560753d9bdbf
Tania Gamero González <tagago1994@gmail.com>
2022-01-18 11:01:34 +0100

    Added select to filter by counselor

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,7 @@ const getAdalabers = () => {
     return fetch('https://beta.adalab.es/pw-recursos/apis/adalabers-v1/promo-patata.json') 
       .then(response => response.json())
       .then(response => {
-       //Limpieza de datos
-       
+       //Limpieza de datos simple
         const results = response.results
         return results;
       });
