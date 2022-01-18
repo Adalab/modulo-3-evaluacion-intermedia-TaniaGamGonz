bf74cd64bd54c44ace67bda8e0a9e60f03265e87
Tania Gamero González <tagago1994@gmail.com>
2022-01-18 09:43:36 +0100

    Added form to add adalabers, first publish on github pages

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,19 @@ import { useEffect, useState } from 'react';
 import getAdalabers from '../services/api'
 
 function App() {
-  //Variables useState
+  //State variables
   const [adalabersData, setAdalabersData] = useState([]);
+  const [adalaber, setAdalaber] = useState({});
 
 
-  //Variables normales
+  //Variables
 
-  //Llamada a la api
+  //Call to api
   useEffect( ()=>getAdalabers().then(response => setAdalabersData(response)) ,[]);
 
-  //Funciones
+  //Functions
 
-
-    //Pintar datos
+    //Render data
     const renderAdalabers = () => {
       return adalabersData.map( adalaber => {
         return(
@@ -27,20 +27,62 @@ function App() {
         )
       })
     }
+    //Events
+    const handleSubmit = (event)=>{
+      event.preventDefault();
+      setAdalabersData([...adalabersData, adalaber]);
+      //Reset the object for the new incoming data
+      setAdalaber({
+        name : '',
+        counselor: '',
+        speciality: '',
+      });
+    }
+    const handleInput = (event)=>{
+      const inputValue = event.target.value;
+      const inputName = event.target.name;
+      setAdalaber({...adalaber, [inputName] : inputValue});
+    }
+    
 
   return (
     <div>
-   <table>
-
-  <thead><tr>
-    <th>Nombre</th>
-    <th>Tutora</th>
-    <th>Especialidad</th>
-  </tr></thead>
-  <tbody>
-    {renderAdalabers()}
-  </tbody>
-</table>
+      <header>
+        <h1>Adalabers</h1>
+      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Tutora</th>
+            <th>Especialidad</th>
+          </tr>
+        </thead>
+        <tbody>
+          {renderAdalabers()}
+        </tbody>
+      </table>
+      <section>
+        <h2>Añadir una adalaber</h2>
+        <form action="" onSubmit={handleSubmit}>
+          <label htmlFor="name">Nombre:
+            <input type="text" name="name" id="name"
+            onChange={handleInput}
+            value={adalaber.name} />
+          </label>
+          <label htmlFor="counselor">Tutora:
+            <input type="text" name="counselor" id="counselor"
+            onChange={handleInput}
+            value={adalaber.counselor} />
+          </label>
+          <label htmlFor="speciality">Especialidad:
+            <input type="text" name="speciality" id="speciality"
+            onChange={handleInput}
+            value={adalaber.speciality} />
+          </label>
+          <button type="submit">Añadir una nueva adalaber</button>
+        </form>
+      </section>
      
     </div>
   );
